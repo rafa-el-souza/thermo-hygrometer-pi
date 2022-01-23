a6b50c06daa44b95909e81e045958dec191968c4
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-01-23 20:35:10 -0300

    Adiciona função para leitura do sensor

---
+++ dht11/index.js
@@ -0,0 +1,20 @@
+const sensor = require("node-dht-sensor").promises;
+
+const MODEL = 11;
+const GPIO = 4;
+
+const readSensor = () => sensor.read(MODEL, GPIO)
+  .then((res) => {
+    const timestamp = Date.now();
+    const datetime = (new Date()).toISOString().slice(0, 19).replace(/-/g, "/").replace("T", " ");
+    console.log(res.temperature, res.humidity, timestamp);
+    console.log(
+      `temp: ${res.temperature.toFixed(1)}°C, ` +
+      `humidity: ${res.humidity.toFixed(1)}%`);
+    return { temperature: res.temperature.toFixed(1), humidity: res.humidity.toFixed(1), timestamp, datetime};
+  })
+  .catch((err) => console.error("Failed to read sensor data:", err));
+
+readSensor();
+
+module.exports = readSensor;
