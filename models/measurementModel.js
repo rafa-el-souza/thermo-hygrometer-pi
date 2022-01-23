c3b13aa9dd9dec00a3adc64a32a15fe1225dc81a
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-01-23 20:37:20 -0300

    Adiciona funções para consulta da última medida e inserção de nova medida no banco de dados

--- models/measurementModel.js
+++ models/measurementModel.js
@@ -1,4 +1,3 @@
-const { execute } = require('./connection');
 const connection = require('./connection');
 
 const getAll = () => {
@@ -11,23 +10,26 @@ const getAll = () => {
     });
 };
 
-/*
- *const getCurrent = () => {
- *`'SELECT * FROM measurement
- *  ORDER BY measurement_id DESC
- *  LIMIT 1;
- * '`
- *};
-*/
+const getLast = () => {
+  connection.execute('SELECT * FROM measurement ORDER BY measurement_id DESC LIMIT 1;')
+    .then(([rows]) => {
+      return rows;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
 
-/*
- *const create = () => {
- *`'INSERT INTO measurement (temperature_value, humidity_value, measured_at)
- * VALUES(?, ?, ?); 
- * '`
- *};
-*/
+const add = (temperature, humidity, timestamp, datetime) => {
+  connection.execute('INSERT INTO measurement (temperature_value, humidity_value, time_stamp, date_time)VALUES(?, ?, ?, ?);', [temperature, humidity, timestamp, datetime])
+    .then(([rows]) => {
+      return rows;
+    })
+    .catch((err) => console.error(err))
+}
 
 module.exports = {
-  getAll
+  getAll,
+  getLast,
+  add
 };
