b1c2cb80aab6958fbb7ba635d07ae1eae7f8a3d7
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-01-23 00:27:00 -0300

    Adiciona rota de teste

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { getAll } = require('./models/measurementModel');
 const app = express();
 
 const PORT = 3000;
+
+app.get('/', getAll);
 
 app.listen(3000, () => {
   console.log(`Listening on port ${PORT}` );
