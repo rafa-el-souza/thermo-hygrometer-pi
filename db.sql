f59c9aaa1d27a4e2744a0ab3edfb36362ad87064
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-01-23 20:34:10 -0300

    Adiciona coluna date_time no banco de dados

--- db.sql
+++ db.sql
@@ -8,12 +8,13 @@ CREATE TABLE measurement(
   measurement_id INT AUTO_INCREMENT PRIMARY KEY,
   temperature_value INT NOT NULL,
   humidity_value INT NOT NULL,
-  measured_at INT NOT NULL
+  time_stamp INT NOT NULL,
+  date_time DATETIME NOT NULL
 ) engine = InnoDB;
 
-INSERT INTO measurement (measurement_id, temperature_value, humidity_value, measured_at)
+INSERT INTO measurement (measurement_id, temperature_value, humidity_value, time_stamp, date_time)
 VALUES
-  (1, 256, 80, 1),
-  (2, 257, 81, 2),
-  (3, 258, 81, 3),
-  (4, 259, 81, 4);
+  (1, 256, 80, 1, "2022/01/01 00:00:00"),
+  (2, 257, 81, 2, "2022/01/01 00:00:05"),
+  (3, 258, 81, 3, "2022/01/01 00:00:10"),
+  (4, 259, 81, 4, "2022/01/01 00:00:15");
