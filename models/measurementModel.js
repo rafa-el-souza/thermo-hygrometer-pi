0d254276f1549849e0860d03f06dacf8ce483ad5
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-01-24 15:22:06 -0300

    Refatora | Instala mariadb | Desinstala mysql2

--- models/measurementModel.js
+++ models/measurementModel.js
@@ -3,65 +3,49 @@ const pool = require('./connection');
 const getAll = () => {
   pool.getConnection()
     .then(conn => {
-
       conn.query('SELECT * FROM measurement')
-        .then(([rows]) => {
-          console.log(rows); //[ {val: 1}, meta: ... ]
-          return rows;
-        })
         .then((res) => {
-          console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+          console.log(res.map((entry) => entry['meta'] ? null : entry));
           conn.end();
         })
         .catch(err => {
           console.error(err);
           conn.end();
         })
-
     }).catch(err => console.error(err));
 };
 
 const getLast = () => {
   pool.getConnection()
     .then(conn => {
-
       conn.query('SELECT * FROM measurement ORDER BY measurement_id DESC LIMIT 1;')
-        .then(([rows]) => {
-          console.log(rows); //[ {val: 1}, meta: ... ]
-          return rows;
-        })
         .then((res) => {
-          console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+          console.log(res.map((entry) => entry['meta'] ? null : entry));
           conn.end();
         })
         .catch(err => {
           console.error(err);
           conn.end();
         })
-
     }).catch(err => console.error(err));
 };
 
 const add = (temperature, humidity, timestamp, datetime) => {
   pool.getConnection()
     .then(conn => {
-
       conn.query('INSERT INTO measurement (temperature_value, humidity_value, time_stamp, date_time)VALUES(?, ?, ?, ?);', [temperature, humidity, timestamp, datetime])
-        .then(([rows]) => {
-          console.log(rows); //[ {val: 1}, meta: ... ]
-          return rows;
-        })
         .then((res) => {
-          console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+          console.log(res, res.insertId);
           conn.end();
         })
         .catch(err => {
           console.error(err);
           conn.end();
         })
-
     }).catch(err => console.error(err));
 }
+
+// getByDateTime
 
 module.exports = {
   getAll,
