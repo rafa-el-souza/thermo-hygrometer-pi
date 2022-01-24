b67ce5d7513aca4fe5ff3aab8fe41aaa62a2fa6e
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-01-24 16:17:21 -0300

    Adiciona função | gravação de dados do sensor no banco de dados

--- dht11/index.js
+++ dht11/index.js
@@ -1,5 +1,7 @@
 const sensor = require("node-dht-sensor").promises;
 
+const { add } = require('../models/measurementModel');
+
 const MODEL = 11;
 const GPIO = 4;
 
@@ -7,16 +9,18 @@ const readSensor = () => sensor.read(MODEL, GPIO)
   .then((res) => {
     const timestamp = Date.now();
     const datetime = (new Date()).toISOString().slice(0, 19).replace(/-/g, "/").replace("T", " ");
-    // console.log(
-    //   `temp: ${res.temperature.toFixed(1)}°C, ` +
-    //   `humidity: ${res.humidity.toFixed(1)}%`);
     return { temperature: res.temperature, humidity: res.humidity, timestamp, datetime};
   })
-  .then((res) => {
-    console.log(res);
-  })
+  // .then((res) => {
+  //   console.log(res);
+  // })
   .catch((err) => console.error("Failed to read sensor data:", err));
 
-setInterval(readSensor, 3000);
+const recordSensor = () => {
+  setInterval(readSensor
+    .then(({ temperature, humidity, timestamp, datetime }) => {
+      add(temperature, humidity, timestamp, datetime)
+    }), 5000);
+};
 
-module.exports = readSensor;
+module.exports = { readSensor, recordSensor};
